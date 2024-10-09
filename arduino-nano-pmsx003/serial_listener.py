a17484243fc453e3240d62a522e6065fae1971b5
Zhomartkhan Talgatuly <zforzesteam@gmail.com>
2024-10-09 21:45:15 +0500

    Update serial listener

--- arduino-nano-pmsx003/serial_listener.py
+++ arduino-nano-pmsx003/serial_listener.py
@@ -3,6 +3,7 @@ import time
 
 import json
 import requests
+import geocoder
 
 SERIAL_PORT = 'COM4'
 SERIAL_BAUD_RATE = 9600
@@ -11,7 +12,7 @@ ADX_API_URL = 'http'
 
 def push_to_server(data):
     try:
-        response = requests.post(ADX_API_URL, data)
+        response = requests.post(ADX_API_URL, json=data)
         if response.status_code == 200:
             print('Response: OK')
         else:
@@ -26,5 +27,12 @@ with serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_BAUD_RATE, timeout=2) as se
         if seriald.in_waiting > 0:
             rawData = seriald.readline().decode('utf-8').rstrip()
             data = json.loads(rawData)
+            location = geocoder.ip('me')
+            if location.ok:
+                data['lat'] = location.latlng[0]  # Get latitude
+                data['lon'] = location.latlng[1]  # Get longitude
+            else:
+                print("Could not get location.")
+            print(data)
             push_to_server(data)
         time.sleep(0.1)
